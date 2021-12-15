2f7b1a29301afda219269da5c2f0c35cb5d7bfa6
atul-dew <arathor@dewsolutions.in>
2021-12-15 11:19:21 +0530

    Add Http logs

--- app/src/main/java/com/example/moviedemo/network/RetrofitBuilder.kt
+++ app/src/main/java/com/example/moviedemo/network/RetrofitBuilder.kt
@@ -1,24 +1,37 @@
 package com.example.moviedemo.network
-
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+
+
 
 object RetrofitBuilder {
     private var logging = HttpLoggingInterceptor()
     val apiService: ApiService by lazy { getRetrofit().create(ApiService::class.java) }
+    val apiServicee: ApiService by lazy { getRetrofit().create(ApiService::class.java) }
+
     private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
     private fun getRetrofit():Retrofit{
-        return Retrofit.Builder()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(logging) // <-- this is the important line!
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(httpClient.build())
+            .build()
+        return retrofit
+       /* return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .client(getHttpLogClient())
-            .build()
+            .build()*/
     }
 
     private fun getHttpLogClient(): OkHttpClient {
